a5b5e6875ff7778ad46021236ad3135523eae9aa
Liubomur Puliak <104902816+liubkkkko@users.noreply.github.com>
2022-06-22 13:23:21 +0300

    Update main.go
    Добавив змінну result. З приводу пул реквестів то я змерджив їх сам, надіюсь, що це не критично

--- Task1/main.go
+++ Task1/main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-    arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
-    fmt.Println("Початковий масив", arr)
-    mapForClear := make(map[int]int)
-    for _, val := range arr {
-      mapForClear[val] = val
-    }
-    var ClearedArr []int
-    for val:= range mapForClear {
-      ClearedArr = append(ClearedArr, val)
-    }
-    fmt.Println("Очищений масив",ClearedArr)
-  }
+	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+	fmt.Println("Початковий масив", arr)
+	mapForClear := make(map[int]int)
+	for _, val := range arr {
+		mapForClear[val] = val
+	}
+	var result []int
+	for val := range mapForClear {
+		result = append(result, val)
+	}
+	fmt.Println("Очищений масив", result)
+}
